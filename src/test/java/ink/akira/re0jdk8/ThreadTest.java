f866e0d28bf592bcc18c945d4fb906944958a3db
akira <chin.kou.akira@gmail.com>
2019-11-26 22:11:58 +0800

    add article about thread

--- src/test/java/ink/akira/re0jdk8/ThreadTest.java
+++ src/test/java/ink/akira/re0jdk8/ThreadTest.java
@@ -1,54 +1,14 @@
 package ink.akira.re0jdk8;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class ThreadTest {
-    public static AtomicInteger ai = new AtomicInteger(0);
-    public static final int LIMIT_COUNT = 30;
-
-    public static class PrintTask implements Runnable{
-        private String printString;
-        private Semaphore current;
-        private Semaphore next;
-
-        public PrintTask(String printString, Semaphore current, Semaphore next) {
-            this.printString = printString;
-            this.current = current;
-            this.next = next;
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    current.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(ai.get() + printString);
-                if (current.availablePermits() == 0) {
-                    next.release(2);
-                }
-                if (ai.getAndIncrement() >= LIMIT_COUNT) {
-                    break;
-                }
-            }
-        }
+    public static void main(String[] args) {
+        new MyThread().start();
     }
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(7);
-        Semaphore s0 = new Semaphore(2);
-        Semaphore s1 = new Semaphore(0);
-        Semaphore s2 = new Semaphore(0);
-        PrintTask a = new PrintTask("A", s0, s1);
-        PrintTask b = new PrintTask("B", s1, s2);
-        PrintTask c = new PrintTask("C", s2, s0);
-        new Thread(a).start();
-        new Thread(b).start();
-        new Thread(c).start();
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("MyThread.run");
+        }
     }
 }
