771d50471335bf059d7e522181c45f0b7f12eecb
akira <chin.kou.akira@gmail.com>
2019-12-02 16:06:33 +0800

    add article about final, serializable, comment

--- src/main/ink/akira/re0jdk8/EmptyClass.java
+++ src/main/ink/akira/re0jdk8/EmptyClass.java
@@ -1,4 +1,22 @@
 package ink.akira.re0jdk8;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class EmptyClass {
+    public static final String ABC;
+
+    static {
+        ABC = "A";
+    }
+
+    public void abc(){
+        try {
+            throw new FileNotFoundException();
+        } catch (FileNotFoundException e) {
+            throw new IOException();
+        } finally {
+            return;
+        }
+    }
 }
